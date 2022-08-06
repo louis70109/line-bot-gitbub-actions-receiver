3257b9c13eaefb263707923f32c834412315b0aa
louis70109 <louis70109@gmail.com>
2022-08-06 19:40:35 +0800

    chore: change if update method condition

--- utils/github.py
+++ utils/github.py
@@ -2,7 +2,7 @@ import requests, base64, os
 from datetime import datetime
 
 
-class Github():
+class Github:
     def __init__(self):
         self.today = datetime.now().strftime("%Y-%m-%d")
         self.repo_name = 'louis70109/ideas-tree'  # change to your repo name
@@ -22,11 +22,12 @@ class Github():
     def new_or_update_record(self, text: str, today_record: str = None, sha: str = None) -> dict:
         git_url = f"https://api.github.com/repos/{self.repo_name}/contents/{self.file}"
 
-        record = base64.b64decode(today_record).decode('UTF-8')
-
-        if today_record != None:
+        # If contain SHA, means content exist.
+        if sha != None:
+            record = base64.b64decode(today_record).decode('UTF-8')
             text = f'{record}<br />{text}'
         update_sha = {'sha': sha} if sha else {}
+
         res = requests.put(
             headers={
                 "Accept": "application/vnd.github.VERSION.raw",
@@ -35,7 +36,7 @@ class Github():
             json={
                 "message": f"✨{self.today} Commit",
                 "committer": {"name": "NiJia Lin", "email": os.getenv('EMAIL')},
-                "content": base64.b64encode(text.encode('UTF-8')).decode('UTF-8'),
+                "content": base64.b64encode(text.encode('UTF-8')).decode('utf-8'),
                 "branch": "master", **update_sha},
             url=git_url
         )
