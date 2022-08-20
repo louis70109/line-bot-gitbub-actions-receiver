b4cc6a4a41f3a677275b612941dd7d3b71c2253f
louis70109 <louis70109@gmail.com>
2022-08-21 00:49:43 +0800

    👣chore: move package layer and add healthz check

--- api.py
+++ api.py
@@ -1,10 +1,10 @@
-from flask import Flask
-from flask_cors import CORS
-from flask_restful import Api
 from dotenv import load_dotenv
 
 load_dotenv()
 
+from flask import Flask
+from flask_cors import CORS
+from flask_restful import Api
 from controller.line_controller import LineIconSwitchController
 import os
 
@@ -12,6 +12,11 @@ app = Flask(__name__)
 CORS(app)
 
 api = Api(app)
+
+@app.get("/healthz")
+def check():
+    return {"message": "Hello World!"}
+
 api.add_resource(LineIconSwitchController, '/webhooks/line')
 
 if __name__ == '__main__':
