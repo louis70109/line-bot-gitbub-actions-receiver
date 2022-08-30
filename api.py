5d9fd24c3a3d15aeb83da8863df890bb6b3411dd
louis70109 <louis70109@gmail.com>
2022-08-30 18:15:27 +0800

    👣chore: add env condition

--- api.py
+++ api.py
@@ -1,19 +1,20 @@
-from dotenv import load_dotenv
+import os
 
-load_dotenv()
+if os.getenv('PY_ENV') != 'production':
+    from dotenv import load_dotenv
+    load_dotenv()
 
 from flask import Flask
 from flask_cors import CORS
 from flask_restful import Api
 from controller.line_controller import LineIconSwitchController
-import os
 
 app = Flask(__name__)
 CORS(app)
 
 api = Api(app)
 
-@app.get("/healthz")
+@app.get("/")
 def check():
     return {"message": "Hello World!"}
 
