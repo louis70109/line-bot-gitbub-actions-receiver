ad3aa37f8707dfc16315a3653aa76a21f2b76e0d
Nijia Lin <louis70109@gmail.com>
2022-11-26 19:13:58 +0800

    ✨feat: add logging

--- api.py
+++ api.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 debug = False
@@ -9,8 +10,10 @@ if os.getenv('PY_ENV') != 'production':
 from flask import Flask
 from flask_cors import CORS
 from flask_restful import Api
-from controller.line_controller import LineIconSwitchController
+from controller.line_controller import LineController
 
+logging.basicConfig(level=os.environ.get("LOG_LEVEL", "INFO"))
+logger = logging.getLogger(__name__)
 app = Flask(__name__)
 CORS(app)
 
@@ -20,7 +23,7 @@ api = Api(app)
 def check():
     return {"message": "Hello World!"}
 
-api.add_resource(LineIconSwitchController, '/webhooks/line')
+api.add_resource(LineController, '/webhooks/line')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=os.getenv('PORT'), debug=debug)
