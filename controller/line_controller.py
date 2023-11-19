1a2aa575d421aaeb48257ba05dff802fdefb4af5
Nijia Lin <louis70109@gmail.com>
2023-11-19 19:35:40 +0800

    ♻️  add side project link to line bot

--- controller/line_controller.py
+++ controller/line_controller.py
@@ -103,6 +103,19 @@ class LineController(Resource):
                 event.reply_token,
                 messages=TextSendMessage(text=reply_text)
             )
+        elif text == '入口':
+            reply_text = '''
+                - QRC: https://custom-qrcode-lpdaqdezra-zf.a.run.app
+                * Bot: https://lin.ee/pNz6HA5
+
+                攝影機: https://lin.ee/YlaqS0t
+                
+                行事曆: https://lin.ee/92O5Od8
+            '''
+            line_bot_api.reply_message(
+                event.reply_token,
+                messages=TextSendMessage(text=reply_text)
+            )
         else:
             github = Github()
             record = github.get_record()
