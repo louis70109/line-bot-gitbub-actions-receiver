1d35e201d64cc63f12948c183fceea5eca328b08
Nijia Lin <louis70109@gmail.com>
2024-07-07 09:36:18 +0800

    🔧 refactor the github api code

--- utils/gemini_service.py
+++ utils/gemini_service.py
@@ -3,7 +3,7 @@ from utils.configmap import Config
 
 class GeminiService:
     def __init__(self):
-        self.model = genai.GenerativeModel('gemini-1.5-pro')
+        self.model = genai.GenerativeModel('gemini-1.5-flash')
         genai.configure(api_key=Config.GEMINI_API_KEY)
 
     def generate_content(self, prompt):
--- utils/github.py
+++ utils/github.py
@@ -3,73 +3,74 @@ import requests
 import base64
 import os
 from datetime import datetime
+
 logger = logging.getLogger(__name__)
 
-
 class Github:
-    def __init__(self):
+    BASE_URL = "https://api.github.com"
+    
+    def __init__(self, repo_name='louis70109/ideas-tree'):
         self.today = datetime.now().strftime("%Y-%m-%d")
-        self.repo_name = 'louis70109/ideas-tree'  # change to your repo name
-        self.file = self.today + '.md'
+        self.repo_name = repo_name
+        self.file = f"{self.today}.md"
+        self.token = os.getenv('GITHUB')
+        self.email = os.getenv('EMAIL')
+
+    def _headers(self, accept="application/vnd.github+json"):
+        return {
+            "Accept": accept,
+            "Authorization": f"token {self.token}"
+        }
+
+    def _get_git_url(self, endpoint):
+        return f"{self.BASE_URL}/repos/{self.repo_name}/contents/{endpoint}"
 
     def get_record(self) -> dict:
-        git_url = f"https://api.github.com/repos/{self.repo_name}/contents/{self.file}"
-        res = requests.get(
-            headers={
-                "Accept": "application/vnd.github+json",
-                "Authorization": f"token {os.getenv('GITHUB')}"
-            },
-            url=git_url
-        )
+        url = self._get_git_url(self.file)
+        res = requests.get(headers=self._headers(), url=url)
+        
         if res.status_code >= 400:
             logger.debug(f'❌ GitHub repo record is None. Info: {res.json()}')
-
+            return {}
+        
         return res.json()
 
     def new_or_update_record(self, text: str, today_record: str = None, sha: str = None) -> dict:
-        git_url = f"https://api.github.com/repos/{self.repo_name}/contents/{self.file}"
-
+        url = self._get_git_url(self.file)
         text = self.markdown_to_html(text)
-
-        update_sha = {}
-        # If contain SHA, means content exist.
-        if sha != None:
+        
+        if sha:
             record = base64.b64decode(today_record).decode('UTF-8')
             text = f'{record}<br />{text}'
-            update_sha = {'sha': sha}
 
-        res = requests.put(
-            headers={
-                "Accept": "application/vnd.github.VERSION.raw",
-                "Authorization": f"token {os.getenv('GITHUB')}"
-            },
-            json={
-                "message": f"✨{self.today} Commit",
-                "committer": {"name": "NiJia Lin", "email": os.getenv('EMAIL')},
-                "content": base64.b64encode(text.encode('UTF-8')).decode('utf-8'),
-                "branch": "master", **update_sha},
-            url=git_url
-        )
+        payload = {
+            "message": f"✨{self.today} Commit",
+            "committer": {"name": "NiJia Lin", "email": self.email},
+            "content": base64.b64encode(text.encode('UTF-8')).decode('utf-8'),
+            "branch": "master"
+        }
+        
+        if sha:
+            payload['sha'] = sha
+
+        res = requests.put(headers=self._headers("application/vnd.github.VERSION.raw"), json=payload, url=url)
+        
         if res.status_code >= 400:
-            logger.warning(
-                f'❌ GitHub Record create || update fail. Info: {res.json()}')
+            logger.warning(f'❌ GitHub Record create || update fail. Info: {res.json()}')
             return None
+        
         return res.json()
 
     @staticmethod
     def markdown_to_html(contents):
+        url = f"{Github.BASE_URL}/markdown"
         res = requests.post(headers={
             "Accept": "application/vnd.github+json",
             "Authorization": f"token {os.getenv('GITHUB')}"
-        }, url="https://api.github.com/markdown",
-            json={"text": contents})
+        }, url=url, json={"text": contents})
 
         if res.status_code == 200:
             return res.text
-        if res.status_code >= 400:
-            logger.warning(f'❌ Markdown format error. Info: {res.json()}')
-            return None
-
-    @staticmethod
-    def record_judgement():
-        pass
+        
+        logger.warning(f'❌ Markdown format error. Info: {res.json()}')
+        return None
